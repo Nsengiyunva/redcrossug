2a7e1f96f937cd5a22a490fe70700b1ac5d8f3f3
unknown <Nsengiyunva>
2025-01-19 13:48:41 +0300

    updates the auth screens

--- lib/scenes/widgets/country_picker.dart
+++ lib/scenes/widgets/country_picker.dart
@@ -38,82 +38,80 @@ class _PhoneNumberField extends State<CountryPicker> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-            padding: const EdgeInsets.symmetric( horizontal: 5 ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric( horizontal: 10 ),
-                  child: Text( widget.question, style: const TextStyle( 
-                    fontSize: 12.74, 
-                    fontFamily: "Inter", 
-                    fontWeight: FontWeight.w500, 
-                    color: AppColors.greyColorC 
-                    )
-                  ) 
+      // final List<String> _items = ['Apple', 'Banana', 'Cherry', 'Date', 'Grape'];
+      // String? _selectedItem;
+
+      return Container(
+        padding: const EdgeInsets.symmetric( horizontal: 5 ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric( horizontal: 10 ),
+              child: Text( widget.question, style: const TextStyle( 
+                fontSize: 12.74, 
+                fontFamily: "Inter", 
+                fontWeight: FontWeight.w500, 
+                color: AppColors.greyColorC 
+                )
+              ) 
+            ),
+            TextField(
+              controller: widget.textEditingController,
+              style: const TextStyle( 
+                fontSize: 16, 
+                fontWeight: FontWeight.w600, 
+                fontFamily: "Manrope", 
+                color: Color(0xFF000000)  
+              ),
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular( 12 ),
+                  borderSide: const BorderSide(color: AppColors.primaryRedColor, width: 1.0),
                 ),
-                TextField(
-                  controller: widget.textEditingController,
-                  style: const TextStyle( 
-                    fontSize: 16, 
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular( 12 ),
+                  borderSide: const BorderSide(color: AppColors.greyColorD, width: 1.0),
+                ),
+                hintText: '',
+                hintStyle: const TextStyle( 
+                    fontSize: 14, 
                     fontWeight: FontWeight.w600, 
                     fontFamily: "Manrope", 
-                    color: Color(0xFF000000)  
+                    color: Color(0xFF929292)  
                   ),
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular( 12 ),
-                      borderSide: const BorderSide(color: AppColors.primaryRedColor, width: 1.0),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular( 12 ),
-                      borderSide: const BorderSide(color: AppColors.greyColorD, width: 1.0),
-                    ),
-                    hintText: '',
-                    hintStyle: const TextStyle( 
-                        fontSize: 14, 
-                        fontWeight: FontWeight.w600, 
-                        fontFamily: "Manrope", 
-                        color: Color(0xFF929292)  
-                      ),
-                    prefixIcon: DropdownButtonHideUnderline(
-                      child: DropdownButton<String>(
-                        value: "Uganda",
-                        icon: const Icon(Icons.arrow_drop_down),
-                        items: nations.map((entry) {
-                          // print( entry["code"] );
-                          // return DropdownMenuItem<String>( 
-                          //   value: entry.key,
-                          //   child: Text('${entry.value}  +${entry.key}'),
-                          // );
-                          return (
-                            DropdownMenuItem<String>( 
-                              value: entry["name"].toString(),
-                              child: Flag(
-                                flagUrl: entry["image" ], 
-                                country_code: entry["name"].toString()
-                              ),
-                            )
-                          );
-                        }).toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedCountry = value!;
-                          } );
-                          // widget.handleTelephoneChange( value! );
-                          // print( value );
-                        },
-                      ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+                prefixIcon: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    value: "Uganda",
+                    icon: const Icon(Icons.arrow_drop_down),
+                    items: nations.map((entry) {
+                      return (
+                        DropdownMenuItem<String>( 
+                          value: entry["name"].toString(),
+                          child: Flag(
+                            flagUrl: entry["image" ], 
+                            country_code: entry["name"].toString()
+                          ),
+                        )
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedCountry = value!;
+                      } );
+                      // widget.handleTelephoneChange( value! );
+                      // print( value );
+                    },
                   ),
-                    keyboardType: TextInputType.phone,
-                  )
-              ],
-            ),
-          );
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+                keyboardType: TextInputType.phone,
+              )
+          ],
+        ),
+      );
   }
 }
